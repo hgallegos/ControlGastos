7747a2a2af76a9d3f67169439e75493f9466927d
Linkunder <linkunder@hotmail.com>
2016-06-14 00:42:35 -0400

    Detallar (falta lo de los elementos)y cambios menores en formgastos

---
+++ Gastos/Detallar.aspx.vb
@@ -0,0 +1,26 @@
+﻿
+Partial Class Gastos_Detallar
+    Inherits System.Web.UI.Page
+
+
+    Protected Sub ButtonDetallar_Click(sender As Object, e As EventArgs) Handles ButtonDetallar.Click
+
+        labelPreguntaDetalle.Text = ""
+        ButtonNoDetallar.Visible = False
+        ButtonDetallar.Visible = False
+        ListBoxElementos.Visible = True
+
+
+
+    End Sub
+
+    Protected Sub ButtonNoDetallar_Click(sender As Object, e As EventArgs) Handles ButtonNoDetallar.Click
+
+    End Sub
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+    End Sub
+
+
+End Class
